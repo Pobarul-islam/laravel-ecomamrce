84dfb6facbc7807ca9f43f3f0dce0e4c84a3d35c
Pobarul-islam <pobarul3@gmail.com>
2022-12-01 18:59:28 +0600

    some code added

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -10,6 +12,7 @@ class UserController extends Controller
 
     function login(Request $req)
     {
-        return $req->input();
+        $user= User::where(['email'=>$req->email])->first();
+        
     }
 }
